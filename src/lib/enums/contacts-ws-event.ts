8513cc540aa6673a56a0668310d498aca3052cc1
fadil-coder <fadilhambali25@gmail.com>
2025-07-05 00:57:01 +0800

    Add ContactWsEvent enum for WebSocket event handling related to contacts

---
+++ src/lib/enums/contacts-ws-event.ts
@@ -0,0 +1,8 @@
+export enum ContactWsEvent {
+  CREATED_CONTACT = "contact:created_contact",
+  CREATE_CONTACT = "contact:create_contact",
+  UPDATE_CONTACT = "contact:update_contact",
+  DELETE_CONTACT = "contact:delete_contact",
+  UPDATED_CONTACT = "contact:updated_contact",
+  DELETED_CONTACT = "contact:deleted_contact",
+}
