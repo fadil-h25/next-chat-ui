23bf3f08060a9aefc6cbf629d19ac4a75409c382
fadil-coder <fadilhambali25@gmail.com>
2025-07-05 00:57:13 +0800

    Implement contact update functionality in ContactDetail component

--- src/components/contact-detail/index.tsx
+++ src/components/contact-detail/index.tsx
@@ -12,10 +12,23 @@ import {
 } from "../ui/sheet";
 
 import useContactStore from "@/store/use-contact-store";
+import { sendUpdateContact } from "@/lib/socket/contact/update-contact";
+import { getSocket } from "@/lib/socket/auth";
+import { useState } from "react";
 
 export function ContactDetail() {
   const isOpen = useContactStore((state) => state.isOpen);
   const selectedContact = useContactStore((state) => state.selectedContact);
+  const [name, setName] = useState("");
+
+  function handleUpdateContact(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("handleUpdateContact", name, selectedContact);
+
+    if (selectedContact?.id !== undefined) {
+      sendUpdateContact(getSocket(), { name, contactId: selectedContact.id });
+    }
+  }
 
   return (
     <Sheet
@@ -29,32 +42,40 @@ export function ContactDetail() {
       }}
     >
       <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
-        </SheetHeader>
-        <div className="grid flex-1 auto-rows-min gap-6 px-4">
-          <div className="grid gap-3">
-            <Label htmlFor="name">name</Label>
-            <Input id="name" name="name" defaultValue={selectedContact?.name} />
+        <form onSubmit={handleUpdateContact}>
+          <SheetHeader>
+            <SheetTitle>Edit profile</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="grid flex-1 auto-rows-min gap-6 px-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name">name</Label>
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                id="name"
+                name="name"
+                defaultValue={selectedContact?.name}
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="">phone</Label>
+              <Input
+                id="phone"
+                name="phone"
+                defaultValue={selectedContact?.target.phone}
+              />
+            </div>
           </div>
-          <div className="grid gap-3">
-            <Label htmlFor="">phone</Label>
-            <Input
-              id="phone"
-              name="phone"
-              defaultValue={selectedContact?.target.phone}
-            />
-          </div>
-        </div>
-        <SheetFooter>
-          <Button type="submit">Save changes</Button>
-          <SheetClose asChild>
-            <Button variant="outline">Close</Button>
-          </SheetClose>
-        </SheetFooter>
+          <SheetFooter>
+            <Button type="submit">Save changes</Button>
+            <SheetClose asChild>
+              <Button variant="outline">Close</Button>
+            </SheetClose>
+          </SheetFooter>
+        </form>
       </SheetContent>
     </Sheet>
   );
